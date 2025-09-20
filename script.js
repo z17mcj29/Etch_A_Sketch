73e2f90dd5e5706c0e128dcdd494f57411037f2b
Michael Johnson <z17.mcj@gmail.com>
2025-09-20 15:16:18 -0600

    testing event listeners, working on logic to add hover

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ for (i = 0; i < divAmount; i++){
 
     const content = document.createElement("div");
     content.classList.add("content" + i);
-    content.textContent = `This is Div ${i}`;
+    content.textContent = `Div ${i}`;
     container.appendChild(content);
 }
+
+//Need to add an effect when I hover over a div with my mouse.
+//I know I need to use something like this.
+//btn.addEventListener('hover', function (e) {
+// console.log(e.target);
+//});
+//I need to do some research on the hover concept and on the e.target concept.
+
+//Found something for manipulating child members.
+// // buttons is a node list. It looks and acts much like an array.
+//const buttons = document.querySelectorAll("button");
+// // we use the .forEach method to iterate through each button
+//buttons.forEach((button) => {
+//alert(button.id);
+//})
+
+const divs = document.querySelectorAll("div");
+divs.forEach((div) => {
+    div.addEventListener("click", ()=> {
+        console.log(div.className)
+    }); 
+});
